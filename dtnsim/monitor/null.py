2a1b6f813de09566ec3285236fba160092261b97
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-08 00:02:37 +0900

    *** empty log message ***

--- dtnsim/monitor/null.py
+++ dtnsim/monitor/null.py
@@ -7,6 +7,19 @@
 # Id: Null.pm,v 1.14 2015/12/28 10:28:46 ohsaki Exp $
 #
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from perlcompat import die
 
 class Null:
