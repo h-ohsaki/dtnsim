6acaaa46682b6100198a52e962579c76ef316695
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-07 23:58:22 +0900

    *** empty log message ***

--- dtnsim/mobility/rwp.py
+++ dtnsim/mobility/rwp.py
@@ -7,6 +7,19 @@
 # Id: RandomWaypoint.pm,v 1.20 2015/12/30 02:54:57 ohsaki Exp $
 #
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from dtnsim.mobility.randomwalk import RandomWalk
 
 class RandomWaypoint(RandomWalk):
