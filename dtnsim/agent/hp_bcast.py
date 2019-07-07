8604682844b41b0116a52eee9c0a328a7ec13a9b
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2019-07-07 23:54:12 +0900

    *** empty log message ***

--- dtnsim/agent/hp_bcast.py
+++ dtnsim/agent/hp_bcast.py
@@ -7,6 +7,19 @@
 # Id: HP_BCAST.pm,v 1.11 2015/12/09 14:45:23 ohsaki Exp $
 #
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from collections import defaultdict
 
 from dtnsim.agent.p_bcast import P_BCAST
