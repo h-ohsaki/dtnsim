828fb373bf4cc1dc3ba9af531333630e20240a1f
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-07 23:55:35 +0900

    *** empty log message ***

--- dtnsim/agent/prophet.py
+++ dtnsim/agent/prophet.py
@@ -7,6 +7,19 @@
 # Id: ProPHET.pm,v 1.8 2016/01/19 17:25:53 ohsaki Exp $
 #
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from collections import defaultdict
 
 from dtnsim.agent.epidemic import Epidemic
