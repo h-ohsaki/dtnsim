21118e6a4a3ba1b1e0f9dd006a4823250235eb25
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-08 00:03:54 +0900

    *** empty log message ***

--- dtnsim/path/line.py
+++ dtnsim/path/line.py
@@ -7,6 +7,19 @@
 # $Id: line.py,v 1.2 2018/10/15 13:00:47 ohsaki Exp ohsaki $
 #
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import graphtools
 
 from dtnsim.path.none import NONE
