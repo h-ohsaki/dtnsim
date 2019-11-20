869a377f1f9e66aa695d9a3526de4eb720f8fd4c
Hiroyuki Ohsaki <code@lsnl.jp>
2019-11-20 17:21:28 +0900

    remove RCS tag

--- dtnsim/path/grid.py
+++ dtnsim/path/grid.py
@@ -4,8 +4,6 @@
 # Copyright (c) 2011-2018, Hiroyuki Ohsaki.
 # All rights reserved.
 #
-# $Id: grid.py,v 1.2 2018/10/15 12:59:08 ohsaki Exp ohsaki $
-#
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,7 +21,7 @@
 import graph_tools
 
 from dtnsim.path.line import Line
-from vector_2d import Vector as V
+from dtnsim.vector import Vector as V
 
 class Grid(Line):
     def __init__(self, *args, **kwargs):
