e38949d4ba1b44709ad4a7db8d99ae0380123175
Hiroyuki Ohsaki <code@lsnl.jp>
2019-11-20 17:21:08 +0900

    add GPL copyright
    remove RCS tag

--- dtnsim/monitor/log.py
+++ dtnsim/monitor/log.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 #
 # A dummy class without simulation monitoring.
-# Copyright (c) 2011-2018, Hiroyuki Ohsaki.
+# Copyright (c) 2011-2019, Hiroyuki Ohsaki.
 # All rights reserved.
 #
-# Id: Log.pm,v 1.12 2015/12/30 02:55:06 ohsaki Exp $
-#
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from dtnsim.monitor.null import Null
 
