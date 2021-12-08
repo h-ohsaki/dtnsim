ae155b01ff56b29ddb2a2217853ea7177dd18020
Hiroyuki Ohsaki <code@lsnl.jp>
2021-12-08 10:04:11 +0900

    version 1.4

--- dtnsim/scheduler.py
+++ dtnsim/scheduler.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = '1.3'
+VERSION = '1.4'
 
 def version():
     return """
